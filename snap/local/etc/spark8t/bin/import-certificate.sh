9ccb3db67edb097cf26361a11e0c39b05018d942
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-06-17 07:16:59 +0000

    Fix Java version

--- snap/local/etc/spark8t/bin/import-certificate.sh
+++ snap/local/etc/spark8t/bin/import-certificate.sh
@@ -9,7 +9,7 @@ then
     echo "Alias $1"
     echo "Certificate path $2"
 
-    ${SNAP}/usr/lib/jvm/java-11-openjdk-amd64/bin/keytool -import -v -alias "$1" -file "$2"  -storepass changeit -noprompt -keystore ${SNAP_DATA}/etc/ssl/certs/java/cacerts
+    ${SNAP}/usr/lib/jvm/java-17-openjdk-amd64/bin/keytool -import -v -alias "$1" -file "$2"  -storepass changeit -noprompt -keystore ${SNAP_DATA}/etc/ssl/certs/java/cacerts
     echo "Certificate imported!"
 else
     echo "Missing alias or path!"
