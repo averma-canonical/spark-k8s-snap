535dea1348463ced4801cf3088f520f29dfd1956
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-07-01 15:46:44 +0000

    Fix

--- snap/local/etc/spark8t/launcher.sh
+++ snap/local/etc/spark8t/launcher.sh
@@ -5,4 +5,4 @@ if [[ ! -n "$KUBECONFIG" ]]; then
   KUBECONFIG="$SNAP_REAL_HOME/.kube/config"
 fi
 
-$KUBE_CONFIG exec $1 --kubeconfig=$KUBECONFIG "$@"
+$KUBECONFIG exec $1 --kubeconfig=$KUBECONFIG "$@"
