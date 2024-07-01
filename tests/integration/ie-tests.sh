3dd8394f1ac815235e777f8b7a186f75228c67a4
Paolo Sottovia <paolo.sottovia@canonical.com>
2024-07-01 14:07:52 +0000

    Add more logging

--- tests/integration/ie-tests.sh
+++ tests/integration/ie-tests.sh
@@ -432,24 +432,66 @@ cleanup_user_failure() {
   cleanup_user 1 spark tests
 }
 
-
+echo -e "##################################"
+echo -e "SETUP TEST"
+echo -e "##################################"
 
 setup_tests
 
+echo -e "##################################"
+echo -e "RUN EXAMPLE JOB"
+echo -e "##################################"
+
 (setup_user_admin_context && test_spark_pi_example && cleanup_user_success) || cleanup_user_failure
+
+echo -e "##################################"
+echo -e "RUN SPARK SHELL JOB"
+echo -e "##################################"
 
 (setup_user_admin_context && test_spark_shell && cleanup_user_success) || cleanup_user_failure
 
+echo -e "##################################"
+echo -e "RUN SPARK SQL JOB WITH S3"
+echo -e "##################################"
+
 (setup_user_admin_context && test_spark_sql_with_s3 && cleanup_user_success) || cleanup_user_failure
+
+echo -e "##################################"
+echo -e "RUN SPARK SQL JOB WITH AZURE ABFSS"
+echo -e "##################################"
 
 (setup_user_admin_context && test_spark_sql_with_azure_abfss && cleanup_user_success) || cleanup_user_failure
 
+echo -e "##################################"
+echo -e "RUN PYSPARK JOB WITH S#"
+echo -e "##################################"
+
 (setup_user_admin_context && test_pyspark_with_s3 && cleanup_user_success) || cleanup_user_failure
+
+echo -e "##################################"
+echo -e "RUN PYSPARK JOB WITH AZURE ABFSS"
+echo -e "##################################"
 
 (setup_user_admin_context && test_pyspark_with_azure_abfss && cleanup_user_success) || cleanup_user_failure
 
+echo -e "##################################"
+echo -e "RUN EXAMPLE JOB WITH S3"
+echo -e "##################################"
+
 (setup_user_admin_context && test_example_job_with_s3 && cleanup_user_success) || cleanup_user_failure
+
+echo -e "##################################"
+echo -e "RUN EXAMPLE JOB WITH AZURE ABFSS"
+echo -e "##################################"
 
 (setup_user_admin_context && test_example_job_with_azure_abfss && cleanup_user_success) || cleanup_user_failure
 
+echo -e "##################################"
+echo -e "RUN EXAMPLE WITH RESTRICTED ACCOUNT"
+echo -e "##################################"
+
 (setup_user_restricted_context && test_restricted_account && cleanup_user_success) || cleanup_user_failure
+
+echo -e "##################################"
+echo -e "END OF THE TEST!"
+echo -e "##################################"
